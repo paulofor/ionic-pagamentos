b07e0f58eecffb1d001c1bebdb65550b71deabb9
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-01-21 16:22:50 -0200

    pagseguro aula1

---
+++ pagseg-checkout-ionic-001/src/providers/pagseguro-pgto-service/pagseguro-pgto-service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the PagseguroPgtoServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PagseguroPgtoServiceProvider {
+
+  constructor(public http: HttpClient) {
+    
+  }
+
+}
