930ab6fc449645106056c2db4258cb3badf67931
Paulo Alexandre <paulofore@gmail.com>
2019-02-05 22:16:46 -0200

    mercado-pago

--- loopback/pagamentos/common/models/pag-seguro.js
+++ loopback/pagamentos/common/models/pag-seguro.js
@@ -1,9 +1,135 @@
 'use strict';
 
 var pagseguro = require('pagseguro');
-var PagSeguro = require('node-pagseguro');
+var PagSeguroNode = require('node-pagseguro');
+const request = require('request');
+const xmlParser = require('xml2json');
+var js2xmlparser = require("js2xmlparser");
+
+
+var MP = require("mercadopago");
+
+
 
 module.exports = function (Pagseguro) {
+
+    /**
+     * 
+     * @param {Function(Error)} callback
+     */
+
+    Pagseguro.MercadoPago = function (callback) {
+        MP..setPublishableKey("APP_USR-f9c3c300-bab0-4ae0-b5a5-e6c68f7d5b46");
+        MP.getIdentificationTypes();
+        callback(null);
+    };
+
+
+
+
+
+    const pagseguro = function (params) {
+        this.payment.mode = 'default';
+        this.payment.method = 'creditCard';
+        this.payment.currency = 'BRL';
+        this.payment.sender.hash = '';
+        this.payment.sender.name = 'Paulo A L Forestieri';
+        this.payment.sender.email = 'paforest1970@gmail.com';
+        this.payment.sender.phone.areaCode = '21';
+        this.payment.sender.phone.number = '992902732';
+        this.payment.sender.documents.type = 'CPF';
+        this.payment.sender.documents.value = '01114740780'
+        this.payment.items.id = 'sku001';
+        this.payment.items.description = 'Assinatura';
+
+    }
+
+    const obj = {
+        'mode': 'default',
+        'method': 'creditCard',
+        'currency': 'BRL',
+        'sender': {
+            'name': 'Paulo A L Forestieri',
+            'email': 'paforest1970@gmail.com',
+            'phone': {
+                'areaCode': '21',
+                'number': '992902732'
+            },
+            'documents': {
+                'type': 'CPF',
+                'value': '01114740780'
+            },
+            'itens': {
+                'id': 'sku001',
+                'description': 'Asssinatura 001'
+            }
+        }
+    }
+
+    const enviaSender = function () {
+        const params = {
+            url: 'https://ws.pagseguro.uol.com.br/v2/transactions?email=paulofore@gmail.com&token=5A0836AD2E5B41B4AD993D27B4A4ED3D',
+            body: js2xmlparser.parse("payment", obj)
+        }
+
+        console.log('Params:', params);
+        request.post(params, function (err, response, body) {
+            if (err) {
+                console.log('Erro:', err);
+            } else if (response.statusCode == 200) {
+                console.log('Resposta(200): ', body);
+                const json = JSON.parse(xmlParser.toJson(body));
+                console.log('JSON - 200', JSON.stringify(json));
+                //this.enviaSender();
+            } else {
+                console.log('Resposta(!200):', body);
+                const json = JSON.parse(xmlParser.toJson(body));
+                console.log('JSON - !200', JSON.stringify(json));
+                console.log(body);
+            }
+        })
+    }
+
+
+    /**
+     * 
+     * @param {Function(Error)} callback
+     */
+
+    Pagseguro.VersaoMaisNova = function (callback) {
+
+
+        //var payment = new PagSeguroNode({
+        //    email: 'paulofore@gmail.com',
+        //    token: 'CB4CBC8D23374F219598172EF26BEC37',
+        //    sandbox: true,
+        //    sandbox_email: '123123123123123@sandbox.pagseguro.com.br'
+        //});
+
+        const params = {
+            url: 'https://ws.pagseguro.uol.com.br/v2/sessions?email=paulofore@gmail.com&token=5A0836AD2E5B41B4AD993D27B4A4ED3D'
+        }
+
+
+        request.post(params, function (err, response, body) {
+            if (err) {
+                console.log('Erro:', err);
+            } else if (response.statusCode == 200) {
+                const json = JSON.parse(xmlParser.toJson(body));
+                console.log('JSON - 200', JSON.stringify(json));
+                enviaSender();
+            } else {
+                const json = JSON.parse(xmlParser.toJson(body));
+                console.log('JSON - !200', JSON.stringify(json));
+                console.log(body);
+            }
+        })
+
+        callback(null);
+    };
+
+
+
 
 
     /**
@@ -14,7 +140,7 @@ module.exports = function (Pagseguro) {
         var pag = new pagseguro({
             email: 'paulofore@gmail.com',
             token: 'CB4CBC8D23374F219598172EF26BEC37',
-            mode : 'sandbox'
+            mode: 'sandbox'
         });
 
         //Configurando a moeda e a referência do pedido
@@ -66,9 +192,9 @@ module.exports = function (Pagseguro) {
         //Enviando o xml ao pagseguro
         pag.send(function (err, res) {
             if (err) {
-                console.log('Erro: ' , err);
+                console.log('Erro: ', err);
             }
-            console.log('Sucesso: ' , res);
+            console.log('Sucesso: ', res);
         });
 
         console.log('Objeto', JSON.stringify(pag));
